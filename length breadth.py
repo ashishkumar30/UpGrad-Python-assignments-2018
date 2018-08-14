c8020db5cf57a19f3ce9f1902be506bb47f216c4
Ashish Kumar <42168687+ashishkumar30@users.noreply.github.com>
2018-08-14 16:58:42 +0530

    Delete length breadth.py

--- length breadth.py
+++
@@ -1,8 +0,0 @@
-a=int(input ('\t \n enter lenth '))
-print ('length is ',a)
-b=int (input (' \t \n enter breadth   '))
-print ('breadth is ',b)
-if  ((a%b)== 0 ):
-     print ("yo! \n \t it's a square")
-else:
-    print('its a rectangle')
